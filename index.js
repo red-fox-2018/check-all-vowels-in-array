ac351c6ac7ab73334aedba30721620fbf3ae924b
Tbfaldhi <tubagusfaldhi@gmail.com>
2018-04-04 16:51:38 +0700

    checkdone

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function generateboard(row,col){
 let result=[]
-  let alphabet ='abcdefghijklmnopqrstuvwxyz'
+  let alphabet ='AIUEOAIUEOAIUFGHYTFGHYTTTTT'
     for (var i = 0; i < row; i++) {
       result.push([])
       for (var j = 0; j < col; j++) {
@@ -13,25 +13,28 @@ let result=[]
 }
 
 function checkVowel(row,col){
-  let check='';
-  let vocal='aiueo'
+  let check=0;
+  let vocal='AIUEO'
   var board = generateboard(row,col)
 
-  for (var i = 0; i < board.length; i++) {
-      let satuan = board[i]
-    for (var j = 0; j < satuan.length; j++) {
+  for (var i = 0; i < row; i++) {
+    for (var j = 0; j < col; j++) {
+      if(board[i+1] !== undefined ){
+       if(board[i][j] =='A' ||board[i][j]=='I'||board[i][j]=='U'||board[i][j]=='E'||board[i][j]=='O'){
+         if(board[i][j+1] =='A'||board[i][j+1]=='I'||board[i][j+1]=='U'||board[i][j+1]=='E'||board[i][j+1]=='O'){
+           if(board[i+1][j] =='A'||board[i+1][j] =='I'||board[i+1][j] =='U'||board[i+1][j] =='E'||board[i+1][j] =='O'){
+            if(board[i+1][j+1] =='A'||board[i+1][j+1] =='I'||board[i+1][j+1] =='U'||board[i+1][j+1] =='E'||board[i+1][j+1] =='O'){
+               check+=1;
+            }
+           }
+         }
+       }
+      }
     }
   }
+
   //console.log(board);
   return check
 }
 
 console.log(checkVowel(5,4));
-
-[
-['A,X,C,Y'],
-['E,O,O,S'],
-['I,U,I,N'],
-['M,Y,O,E'],
-['P,D,A,I']
-]
--- retry.js
+++ retry.js
@@ -15,19 +15,35 @@ return result
 
 function checkVowel(input){
   let check=[];
-  let vocal='aiueo'
+  let count=0
+ //count+=1;
+  let vocal=['A','I','U','E','O']
   var board = generateboard(input)
-
+  console.log(board);
   for (var i = 0; i < board.length; i++) {
-    let satuan = board[i]
-    check.push([])
-    for (var j = 0; j < satuan.length; j++) {
-      check[i].push([i,j])
-    }
-  }
-
-
-return check
+    board[i]
+    for (var j = 0; j < board[i].length; j++) {
+      //console.log('++',board[i+1][j]);
+      if(board[i+1] !== undefined ){
+       if(board[i][j] =='A' ||board[i][j]=='I'||board[i][j]=='U'||board[i][j]=='E'||board[i][j]=='O'){
+         if(board[i][j+1] =='A'||board[i][j+1]=='I'||board[i][j+1]=='U'||board[i][j+1]=='E'||board[i][j+1]=='O'){
+           if(board[i+1][j] =='A'||board[i+1][j] =='I'||board[i+1][j] =='U'||board[i+1][j] =='E'||board[i+1][j] =='O'){
+            if(board[i+1][j+1] =='A'||board[i+1][j+1] =='I'||board[i+1][j+1] =='U'||board[i+1][j+1] =='E'||board[i+1][j+1] =='O'){
+               count+=1;
+            }
+           }
+         }
+       }
+      }
+     }
+   }
+return count
 }
+//
+// [ [ [ 0, 0 ], [ 0, 1 ], [ 0, 2 ], [ 0, 3 ] ],
+//   [ [ 1, 0 ], [ 1, 1 ], [ 1, 2 ], [ 1, 3 ] ],
+//   [ [ 2, 0 ], [ 2, 1 ], [ 2, 2 ], [ 2, 3 ] ],
+//   [ [ 3, 0 ], [ 3, 1 ], [ 3, 2 ], [ 3, 3 ] ],
+//   [ [ 4, 0 ], [ 4, 1 ], [ 4, 2 ], [ 4, 3 ] ] ]
 
 console.log(checkVowel('AXCY,EOOS,IUIN,MYOE,PDAI'));
